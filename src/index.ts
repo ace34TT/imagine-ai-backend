7ea487c6a5635de090fded32687a2c7ec0e27c80
Rabenandrasana Tafinasoa <tafinasoa35@gmail.com>
2023-10-02 12:03:56 +0300

    remove chron

--- src/index.ts
+++ src/index.ts
@@ -4,10 +4,10 @@ import axios from "axios";
 const port = process.env.PORT || 3000;
 import cron from "node-cron";
 
-cron.schedule("*/10 * * * *", function () {
-  console.log("running a task every 10 ");
-  axios.get("https://imagine-ai-lrns.onrender.com");
-});
+// cron.schedule("*/10 * * * *", function () {
+//   console.log("running a task every 10 ");
+//   axios.get("https://imagine-ai-lrns.onrender.com");
+// });
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);

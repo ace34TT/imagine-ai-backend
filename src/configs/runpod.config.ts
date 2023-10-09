a31ffd490b1adcf806f86e7eb238c797028ec749
Rabenandrasana Tafinasoa <tafinasoa35@gmail.com>
2023-10-09 19:03:21 +0300

    migration

--- src/configs/runpod.config.ts
+++ src/configs/runpod.config.ts
@@ -1,3 +1,5 @@
 const sdk = require("api")("@runpod/v1.0#18nw21lj8lwwiy");
-sdk.auth("E7K20UBU7T1GYLOFNIYEEMEF8O5W4Y4LW3X0XJC2");
+require("dotenv").config();
+
+sdk.auth(process.env.RUNPOD_AUTH);
 export { sdk };
--- src/helpers/firebase.helper.ts
+++ src/helpers/firebase.helper.ts
@@ -2,15 +2,15 @@ import * as admin from "firebase-admin";
 import path from "path";
 const tempDirectory = path.resolve(__dirname, "../tmp/");
 import fs from "fs";
+
+const { privateKey } = JSON.parse(process.env.FIREBASE_PRIVATE_KEY || "");
 admin.initializeApp({
   credential: admin.credential.cert({
-    projectId: "imagination-station-d9e2c",
-    clientEmail:
-      "firebase-adminsdk-x22yx@imagination-station-d9e2c.iam.gserviceaccount.com",
-    privateKey:
-      "-----BEGIN PRIVATE KEY-----\nMIIEvwIBADANBgkqhkiG9w0BAQEFAASCBKkwggSlAgEAAoIBAQCmPfwtiRRWqERZ\n85r+G2FtCbHH2xgPOMHZ034B/xp1vxYfRsT03TRFpvKD4UxWKKeGkIbWFHCv8yWH\noTqz2oRREeMAS/7qvRnFat+Gh1rfiCZpAE1DPC5seIU6wIygbpd/2Ws7fCz1PONQ\nXavcRXEQs7f+6iLoyFQGxfbXAWL/0LT2/+TCLPFxysXifxXv6hLsdU3HW7O57sR3\n3NJibg4r2GxRERdUblRN96+gWZUe70sW+HVx5dVjHFpcbOAUC4v1zx1suqN6Kdmy\nHgfeyoQ2sBC/gMLQt3byVK0tFhZvPmyicgdja4TUQujj/TUcpHokn0XN1aAISTHX\nl7rFliI7AgMBAAECggEAGPWCeJW1o4MU7ApqkU8sE/JaKj7nQFXNqPYbW5Jg9x+u\n4xg9duQFNpXnoExBqUfmSwpE7DxaZNhVIKVgymL67a8JTWCbj8O/bEfs8mY9CwWG\nespMT1VoROndWuJQ2r9v6IaMlnxAxbsUB9tgc6xLWu4YqGGfxyJYl0G+my4+nliJ\n9lY3ZO6/ZdYtEfMzcHy86ZbaEnsS2LfCcvP60cNa355cwOxbMQxl1WurBJedhJZv\n6g8wsN/zfLPdfarKxWS0gSJL9u7/0lfhk42OSxab0J86dyiR+T4LO1ksDBps+Q5D\njaMBOo8OJYqL5ey3BOn2MWQaDsJH9OTOQA6h9QOhoQKBgQDR7hKBE0AH9DdEq06e\nQrjScOxzzYQ8rGyYKlCmQiDupp8gxUyK7ZpMTDJ25GPd1ZcGlhW54gxzoSFyKCRv\nxIrUWCFRyIXwJ9CqvmxSo5cpq78k4fWAp3SmFPzkBW4Ua7BORqFftbYIgEAVv0vh\nIMqB/x6glOzvD5k4cpMzegQ74QKBgQDKuYRZs6GEfPMpKzbU59grmE2a8JAGd5RU\nOKuYMmerptvAkMyfuOKn/mas4gh4xiOo9Rp+rdf+Vy6Irgmy7HsCUQydLljfDF5q\nP2o3MsYBGe6/IZDmnDTLBsdGGOEF/rxZV9U2KYTrInFItFvcnJ1VIx0/HrNPhJ27\nbJ4sygoBmwKBgQCcOyYwQyhDAx/ije7LXvJ3QqzmHcvM77E9qTguE0a49O22EL8/\nIUbUg1yHGL+txxhHQYjXtn/3ybG/aFp9h3KeRxw85hOvv6HOgDrTYYXM8FEnXLY9\nUeJA+5udAuGCdwZVqsDMHspjFzcaOCx7VdD/3+JmNY8C9sPbu4ELLUJbYQKBgQDH\nM5B3zGk0FMujcpFImCD0hY4MZ8I+eJZKQ/1pam4wX5+kEyTe9f/Sb8tYqisE8j3s\nMhy8yfpqNCTHraxeIr2kwxuQABDX7AGCiZnHb57dSiwtp8AIuwmtTpXVrXDAQVW2\nvPYHaLjxjECg3ok9uK9pDz4X3gT4aCnOF6c9/7HhbQKBgQCwz9eRrsOyzccaZvk3\n1VNcc16r3Ux7y7bTtBXvf2DLZ2aVh9fc1lxVxkM60T+kgCWr5VJRT1lLgRVqtDP0\nZFcLNf/OKx+UIptKirthLCWWyZVkCJpMqFwCJpwqOzbUjC9mcq9P+ehQX7PucFJT\n/JDD7/8PGyFyaFIj6uee+w/Tig==\n-----END PRIVATE KEY-----\n",
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    privateKey,
   }),
-  storageBucket: "gs://imagination-station-d9e2c.appspot.com",
+  storageBucket: process.env.FIREBSE_STORAGE_BACKET,
 });
 
 export const uploadFileToFirebase = async (file: any, filename: string) => {
